25f8882f8d6f711fb5cc53649a71e8d512d49ce7
pxiol <pabliitomartinez@outlook.com>
2014-03-07 12:47:43 -0600

    2.0

--- rpctest/core/views.py
+++ rpctest/core/views.py
@@ -1,56 +1,26 @@
-#from django.db.utils import IntegrityError
-#from django.views.decorators.csrf import csrf_exempt
-
-#from spyne.error import ResourceNotFoundError, ResourceAlreadyExistsError
-#from spyne.server.django import DjangoApplication
 from spyne.model.primitive import Integer
-#from spyne.model.complex import Iterable
-from spyne.service import ServiceBase
 from spyne.protocol.soap import Soap11
-from spyne.application import Application
+from spyne.service import ServiceBase
 from spyne.decorator import rpc
-#from spyne.util.django import DjangoComplexModel
-
-#from rpctest.core.models import FieldContainer
 
 
-#class Container(DjangoComplexModel):
-  #  class Attributes(DjangoComplexModel.Attributes):
-   #     django_model = FieldContainer
-    #    django_exclude = ['excluded_field']
-
-
-class api(ServiceBase):
+class Operaciones(ServiceBase):
     @rpc(Integer, Integer, Integer)
-    def say_hello(ctx, a, b, c):
-        c = a+b
-        return c    
+    def suma(ctx, num1, num2, resul):
+        resul = num1 + num2
+        return resul
 
-class api2(ServiceBase):
-    @rpc(Integer, Integer, Integer)
-    def say_hello2(ctx, a, b, c):
-        c = a-b
-        return c    
+    @rpc(Integer, Integer, Integer)     
+    def rest(ctx, num1, num2, resul):
+        resul = num1 - num2
+        return resul 
 
-#class ContainerService(ServiceBase):
- #   @rpc(Integer, _returns=Container)
-  #  def get_container(ctx, pk):
-   #     try:
-  #          return FieldContainer.objects.get(pk=pk)
-   #     except FieldContainer.DoesNotExist:
-    #        raise ResourceNotFoundError('Container')
+    @rpc(Integer, Integer, Integer)     
+    def mult(ctx, num1, num2, resul):
+        resul = num1 * num2
+        return resul        
 
-   # @rpc(Container, _returns=Container)
-  #  def create_container(ctx, container):
- #       try:
-#            return FieldContainer.objects.create(**container.as_dict())
-#        except IntegrityError:
- #           raise ResourceAlreadyExistsError('Container')
-
-#app = Application([api, ContainerService],
-  #  'app_operaciones',
- #   in_protocol=Soap11(validator='lxml'),
- #   out_protocol=Soap11(),
-#)
-
-#hello_world_service = csrf_exempt(DjangoApplication(app))
+    @rpc(Integer, Integer, Integer)     
+    def divi(ctx, num1, num2, resul):
+        resul = num1 / num2
+        return resul 
--- rpctest/urls.py
+++ rpctest/urls.py
@@ -1,19 +1,14 @@
 from django.conf.urls import patterns, url
-from spyne.protocol.soap import Soap11
 from spyne.server.django import DjangoView
-from rpctest.core.views import api, api2
+from rpctest.core.views import Operaciones
+from spyne.protocol.soap import Soap11
 
 
 urlpatterns = patterns('',
-    #url(r'^hello_world/','core.views.hello_world_service'),
     
-    url(r'^say_hello/', DjangoView.as_view(
-        services=[api], tns='app_operaciones',
+    url(r'^operaciones/', DjangoView.as_view(
+        services=[Operaciones], tns='Operaciones',
         in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())),
 
-    url(r'^say_hello2/', DjangoView.as_view(
-        services=[api2], tns='app_operaciones',
-        in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())),
-    
-   # url(r'^api/', DjangoView.as_view(application=app)),
+      
 )
