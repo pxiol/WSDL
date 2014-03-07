88f41434eb16f4a15af248b9c5896bbbce9fe9a9
pxiol <pabliitomartinez@outlook.com>
2014-03-07 12:23:42 -0600

    ajustando

--- rpctest/core/views.py
+++ rpctest/core/views.py
@@ -1,51 +1,56 @@
-from django.db.utils import IntegrityError
-from django.views.decorators.csrf import csrf_exempt
+#from django.db.utils import IntegrityError
+#from django.views.decorators.csrf import csrf_exempt
 
-from spyne.error import ResourceNotFoundError, ResourceAlreadyExistsError
-from spyne.server.django import DjangoApplication
-from spyne.model.primitive import Unicode, Integer
-from spyne.model.complex import Iterable
+#from spyne.error import ResourceNotFoundError, ResourceAlreadyExistsError
+#from spyne.server.django import DjangoApplication
+from spyne.model.primitive import Integer
+#from spyne.model.complex import Iterable
 from spyne.service import ServiceBase
 from spyne.protocol.soap import Soap11
 from spyne.application import Application
 from spyne.decorator import rpc
-from spyne.util.django import DjangoComplexModel
+#from spyne.util.django import DjangoComplexModel
 
-from rpctest.core.models import FieldContainer
-
-
-class Container(DjangoComplexModel):
-    class Attributes(DjangoComplexModel.Attributes):
-        django_model = FieldContainer
-        django_exclude = ['excluded_field']
+#from rpctest.core.models import FieldContainer
 
 
-class HelloWorldService(ServiceBase):
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def say_hello(ctx, name, times):
-        for i in xrange(times):
-            yield 'Hello, %s' % name
+#class Container(DjangoComplexModel):
+  #  class Attributes(DjangoComplexModel.Attributes):
+   #     django_model = FieldContainer
+    #    django_exclude = ['excluded_field']
 
 
-class ContainerService(ServiceBase):
-    @rpc(Integer, _returns=Container)
-    def get_container(ctx, pk):
-        try:
-            return FieldContainer.objects.get(pk=pk)
-        except FieldContainer.DoesNotExist:
-            raise ResourceNotFoundError('Container')
+class api(ServiceBase):
+    @rpc(Integer, Integer, Integer)
+    def say_hello(ctx, a, b, c):
+        c = a+b
+        return c    
 
-    @rpc(Container, _returns=Container)
-    def create_container(ctx, container):
-        try:
-            return FieldContainer.objects.create(**container.as_dict())
-        except IntegrityError:
-            raise ResourceAlreadyExistsError('Container')
+class api2(ServiceBase):
+    @rpc(Integer, Integer, Integer)
+    def say_hello2(ctx, a, b, c):
+        c = a-b
+        return c    
 
-app = Application([HelloWorldService, ContainerService],
-    'spyne.examples.django',
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11(),
-)
+#class ContainerService(ServiceBase):
+ #   @rpc(Integer, _returns=Container)
+  #  def get_container(ctx, pk):
+   #     try:
+  #          return FieldContainer.objects.get(pk=pk)
+   #     except FieldContainer.DoesNotExist:
+    #        raise ResourceNotFoundError('Container')
 
-hello_world_service = csrf_exempt(DjangoApplication(app))
+   # @rpc(Container, _returns=Container)
+  #  def create_container(ctx, container):
+ #       try:
+#            return FieldContainer.objects.create(**container.as_dict())
+#        except IntegrityError:
+ #           raise ResourceAlreadyExistsError('Container')
+
+#app = Application([api, ContainerService],
+  #  'app_operaciones',
+ #   in_protocol=Soap11(validator='lxml'),
+ #   out_protocol=Soap11(),
+#)
+
+#hello_world_service = csrf_exempt(DjangoApplication(app))
--- rpctest/urls.py
+++ rpctest/urls.py
@@ -1,15 +1,19 @@
 from django.conf.urls import patterns, url
 from spyne.protocol.soap import Soap11
 from spyne.server.django import DjangoView
-from rpctest.core.views import app, HelloWorldService
+from rpctest.core.views import api, api2
 
 
 urlpatterns = patterns('',
-    url(r'^hello_world/','core.views.hello_world_service'),
+    #url(r'^hello_world/','core.views.hello_world_service'),
     
     url(r'^say_hello/', DjangoView.as_view(
-        services=[HelloWorldService], tns='spyne.examples.django',
+        services=[api], tns='app_operaciones',
+        in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())),
+
+    url(r'^say_hello2/', DjangoView.as_view(
+        services=[api2], tns='app_operaciones',
         in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())),
     
-    url(r'^api/', DjangoView.as_view(application=app)),
+   # url(r'^api/', DjangoView.as_view(application=app)),
 )
